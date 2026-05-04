fd59eed6927d66d8f6a855ce3e96c23ab5803ae8
Prisha Sanyal-Aich <psanyalaich@gmail.com>
2026-05-04 07:28:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+
+my_label.pack()
+
+
+import turtle
+
+tim = turtle.Turtle()
+tim.write()
+
+
+window.mainloop()
